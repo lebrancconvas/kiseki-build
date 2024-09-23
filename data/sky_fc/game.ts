ce238b542d1996996b2f449f5bb367cefc2d3f20
lebrancconvas <lebrancconvas@gmail.com>
2024-09-23 23:44:16 +0700

    ✨ feat(data): update estelle data

---
+++ data/sky_fc/game.ts
@@ -0,0 +1,19 @@
+import {Art, Character, Element, Game, Quartz, Stats, StatsChangeType} from '../../proto/gen/kiseki/v1/data_pb';
+
+export const Characters = [
+  Character.create({
+    name: 'Estelle Bright',
+    hp: Math.floor(8*9),
+    str: Math.floor(0.05*16*17+20),
+    def: Math.floor(0.04*16*17),
+    ats: Math.floor(0.05*16*17),
+    adf: 15,
+    dex: Math.floor(0.001*16*17+16),
+    agl: Math.floor(0.001*16*17+4),
+    mov: 4,
+    spd: Math.floor(0.0025*16*17+10),
+    lines: [
+      
+    ]
+  })
+] as Character[];
